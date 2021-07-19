ad455319e511edf4cdb2e8263d6773bd293a618f
tieje <toj320@gmail.com>
2021-07-19 16:04:58 -0400

    figured out 2

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -12,7 +12,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	if (!parent || !original_right)
 		return NULL;
-	right_node->right = NULL;
+	right_node->left = NULL;
 	right_node->right = NULL;
 	right_node->parent = parent;
 	right_node->n = value;
