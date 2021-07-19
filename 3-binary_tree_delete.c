414ac51a0aaa515e1c9dd2da89036d2a53d5c39b
keysmusician <justin.masayda@gmail.com>
2021-07-19 19:35:48 +0000

    Add task 3

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,10 +1,15 @@
 #include "binary_trees.h"
 /**
- * binary_tree_delete - creates a binary tree node
- * @tree: tree
+ * binary_tree_delete - deletes an entire binary tree
+ * @tree: tree root node pointer
  */
-
 void binary_tree_delete(binary_tree_t *tree)
 {
+	if (!tree)
+		return;
 
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+
+	free(tree);
 }
