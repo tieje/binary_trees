0e6e249d79ef31f3d795ea31c51f69d18ddce53a
keysmusician <justin.masayda@gmail.com>
2021-07-20 17:56:24 +0000

    Add task 11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_size - creates a binary tree node
+ * binary_tree_size - measures the size of a binary tree
  * @tree: tree
- * Return: size of nodes
+ * Return: count of all nodes below and including `tree`
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-
+	if (tree == NULL)
+		return (0);
+	return (binary_tree_size(tree->left) +
+		binary_tree_size(tree->right) + 1
+	);
 }
