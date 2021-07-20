eb643d104d8a19fbf0a41191d2559f47980918bb
tieje <toj320@gmail.com>
2021-07-20 12:27:16 -0400

    completed 9

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,26 @@
 #include "binary_trees.h"
+#define MAX(a,b)  (a < b ? b : a)
 /**
- * binary_tree_height - creates a binary tree node
+ * binary_tree_height_helper - measure the height of a binary tree
+ * @tree: tree
+ * Return: size of nodes
+ */
+size_t binary_tree_height_helper(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return 0;
+	left_height = binary_tree_height_helper(tree->left);
+	right_height = binary_tree_height_helper(tree->right);
+	return (MAX(left_height, right_height) + 1);
+}
+/**
+ * binary_tree_height - measure the height of a binary tree
  * @tree: tree
  * Return: size of nodes
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-
+	return (binary_tree_height_helper(tree) - 1);
 }
