6e3428c9bb1f4f8efc1ceb399f1ad994710fac14
keysmusician <justin.masayda@gmail.com>
2021-07-20 20:01:36 +0000

    Add task 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,33 @@
 #include "binary_trees.h"
+#define MAX(a, b) (a < b ? b : a)
+
 /**
- * binary_tree_balance - creates a binary tree node
+ * binary_tree_height_helper - measures the height of a binary tree
  * @tree: tree
- * Return: integer
+ * Return: height of a binary tree
+ */
+size_t binary_tree_height_helper(const binary_tree_t *tree)
+{
+	size_t left_height, right_height;
+
+	if (tree == NULL)
+		return (0);
+	left_height = binary_tree_height_helper(tree->left);
+	right_height = binary_tree_height_helper(tree->right);
+	return (MAX(left_height, right_height) + 1);
+}
+
+/**
+ * binary_tree_balance - measures the balance factor of a binary tree
+ * @tree: tree
+ * Return: balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+	return (binary_tree_height_helper(tree->left) -
+		binary_tree_height_helper(tree->right)
+	);
 }
