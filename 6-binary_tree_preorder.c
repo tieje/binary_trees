71427ddb6186b216251def31d8685448b037390d
tieje <toj320@gmail.com>
2021-07-20 11:31:03 -0400

    6 complete

--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -1,11 +1,15 @@
 #include "binary_trees.h"
 /**
- * binary_tree_preorder - creates a binary tree node
+ * binary_tree_preorder - traverse binary tree with pre-order traversal
  * @tree: tree
  * @func: function
  */
 
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-
+	if (tree == NULL)
+		return;
+	func(tree->n);
+	binary_tree_preorder(tree->left, func);
+	binary_tree_preorder(tree->right, func);
 }
